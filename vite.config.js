7d1125258a67b4e5bde1c26d84c7857f79706449
noqhz <jfritsch.pro@gmail.com>
2025-05-05 02:52:42 +0200

    deploy app - config vite.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Projet-5---Creez-une-application-web-de-location-immobiliere-avec-React/',
   plugins: [react()],
-})
+});
